a355ac46cb68dc5408b744c3239eb8964838c10a
Michal Chalany <mikaelchalany@gmail.com>
2021-06-18 08:10:17 +0200

    Add separate products and offers list resources

--- app/app.py
+++ app/app.py
@@ -6,26 +6,35 @@ import requests
 import falcon
 from peewee import fn
 
-from db.model import Product, OfferPrice
+from db.model import Product, OfferPrice, Offer
 import settings
 
 
-class ProductResource:
+class ProductListResource:
 
-    def on_get(self, req, resp, id):
+    def on_get(self, req, resp):
+        resp.media = {
+            'products': [],
+        }
         try:
-            product = Product.select(
-                Product.id, Product.name, Product.description
-            ).where(Product.id==id).get()
-        except Product.DoesNotExist:
+            products = Product.select().execute()
+            logging.info(products)
+        except:
+            logging.info('error in products get list')
+
+        if not products:
             raise falcon.HTTPNotFound
 
+        for product in products:
+            resp.media['products'].append({
+                'id': product.id,
+                'name': product.name,
+                'description': product.description,
+            })
         resp.status = falcon.HTTP_200
-        resp.media = {
-            'id': product.id,
-            'name': product.name,
-            'description': product.description
-        }
+
+
+class ProductResource:
 
     def on_post(self, req, resp):
         name = req.media.get('name', None)
@@ -49,12 +58,28 @@ class ProductResource:
             "name": name,
             "description": description
         }
-        result = requests.post(
-            settings.PRODUCT_REG_URL,
-            headers=headers,
-            json=payload
-        )
+        # result = requests.post(
+        #     settings.PRODUCT_REG_URL,
+        #     headers=headers,
+        #     json=payload
+        # )
         logging.info(result)
+
+    def on_get(self, req, resp, id):
+        try:
+            product = Product.select(
+                Product.id, Product.name, Product.description
+            ).where(Product.id==id).get()
+        except Product.DoesNotExist:
+            raise falcon.HTTPNotFound
+
+        resp.status = falcon.HTTP_200
+        resp.media = {
+            'id': product.id,
+            'name': product.name,
+            'description': product.description
+        }
+
 
     def on_patch(self, req, resp, id):
         if not req.media:
@@ -71,6 +96,29 @@ class ProductResource:
 
         if not result:
             raise falcon.MediaNotFoundError("id")
+
+
+class OfferListResource:
+
+    def on_get(self, req, resp, product_id):
+        resp.media = {
+            'offers': [],
+        }
+
+        offers = Offer.select().where(
+            Offer.product_id==product_id
+        ).execute()
+
+        if not offers:
+            raise falcon.HTTPNotFound
+
+        for offer in offers:
+            resp.media['offers'].append({
+                'id': offer.id,
+                'price': offer.price,
+                'items_in_stock': offer.items_in_stock,
+            })
+        resp.status = falcon.HTTP_200
 
 
 class PriceTrendResource:
@@ -130,7 +178,8 @@ logging.basicConfig(level=logging.INFO)
 
 app = falcon.App()
 
-app.add_route('/v1/products', ProductResource())
+app.add_route('/v1/products', ProductListResource())
 app.add_route('/v1/products/{id:int}', ProductResource())
+app.add_route('/v1/offers/{product_id:int}', OfferListResource())
 app.add_route('/v1/offer_prices/{id:int}/trend', PriceTrendResource())
 app.add_route('/v1/offer_prices/{id:int}/percentage', PriceChangeResource())
